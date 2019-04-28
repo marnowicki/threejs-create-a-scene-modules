9e4dc37b57178b0dc4adfa49940a26c6392b03f6
Mariusz Nowicki <marnowicki@wp.pl>
2019-04-28 20:59:13 +0200

    added example for loading legacy js code (WebGL.js), detect for webGL availability

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import * as THREE from 'three';
+import * as WEBGL from 'exports-loader?WEBGL!three/examples/js/WebGL.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer= new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1,1,1);
 const material = new THREE.MeshNormalMaterial();
@@ -20,4 +20,12 @@ const animate = () => {
 
     renderer.render(scene, camera);
 }
-animate();
+
+if(WEBGL.isWebGLAvailable()){
+    document.body.appendChild(renderer.domElement);
+    animate();
+}else{
+    var warning = WEBGL.getWebGLErrorMessage();
+    document.body.appendChild( warning );
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
+
+
+
 module.exports = {
     entry: './src/main.js',
     mode: 'development',
