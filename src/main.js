bb53b3357adde853368b259106e20d925e7e9a0f
Mariusz Nowicki <marnowicki@wp.pl>
2019-04-29 20:41:17 +0200

    use of plane, enter: toggles animation

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
-import { Cube } from './objects/cube';
 import { Engine } from './engine';
+import { Cube } from './objects/cube';
+import { Plane } from './objects/plane';
 
 const engine = new Engine();
 document.body.appendChild(engine.domElement);
 if(engine.webGLAvaliable){
-    engine.add(new Cube);
-    engine.start();
+    engine.add(new Cube({size: 1, position: {x:0, y:1, z:0}}));
+    engine.add(new Plane({size: 5}));
+    engine.start(100);
 }
-
+window.addEventListener('keydown', (e)=> {
+    if(e.key === 'Enter'){
+        engine.toggle();
+    }
+});
 
 
 
